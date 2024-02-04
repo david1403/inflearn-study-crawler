36befc4bf2a35551e6a37854ca4e2bc53c471000
최대훈 <david1403@MyHostname.com>
2024-02-04 12:26:34 +0900

    first commit for inflearn study cralwer using spring batch

---
+++ src/main/java/io/springbatch/inflearnstudycrawlerbatch/InflearnStudyCrawlerBatchApplication.java
@@ -0,0 +1,14 @@
+package io.springbatch.inflearnstudycrawlerbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnStudyCrawlerBatchApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnStudyCrawlerBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/springbatch/inflearnstudycrawlerbatch/InflearnStudyCrawlerBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbatch.inflearnstudycrawlerbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnStudyCrawlerBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
